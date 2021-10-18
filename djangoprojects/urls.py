35b8fb153e64189cadfd403cbc814dfb611dd7e7
Balaji-ka <73286859+Balaji-ka@users.noreply.github.com>
2021-10-18 16:11:50 +0530

    Update urls.py

--- djangoprojects/urls.py
+++ djangoprojects/urls.py
@@ -22,8 +22,8 @@ urlpatterns = [
     #path('',include('rootapp.urls')),
     path('admin/', admin.site.urls),
     path('', include('calculatorapp.urls'))
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    #url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    #url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+#urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
